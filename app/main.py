9cc1761da235656a5a6867f9e2cfd75e94525a32
vi280708ovv <vi280708ovv@gmail.com>
2026-07-05 17:03:28 +0300

    stack for jenkins

---
+++ app/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+import psycopg2
+import os
+import time
+
+app = FastAPI()
+
+DB_HOST = os.getenv("DB_HOST", "db")
+DB_NAME = os.getenv("DB_NAME", "devops_db")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "mamba_mentality")
+
+
+@app.get('/')
+def read_root():
+    try:
+        conn=psycopg2.connect(host=DB_HOST, database=DB_NAME, password=DB_PASSWORD,
+                              user=DB_USER,)
+        conn.close()
+        db_status = "Успішно підключено до PostgreSQL!"
+    except Exception as e:
+        db_status = f"Помилка підключення до БД: {str(e)}"
+    return {
+        "status": "Додаток працює",
+        "database": db_status
+    }
+        
