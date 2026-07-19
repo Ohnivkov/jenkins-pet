285d9fe4d767e89b7a6dca574bdb8dc1dad30bee
vi280708ovv <vi280708ovv@gmail.com>
2026-07-19 18:03:21 +0300

    new

--- app/main.py
+++ app/main.py
@@ -1,27 +1,44 @@
-from fastapi import FastAPI
-import psycopg2
 import os
-import time
+from fastapi import FastAPI, Request, Depends
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from dotenv import load_dotenv
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
-app = FastAPI()
+load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST", "db")
-DB_NAME = os.getenv("DB_NAME", "devops_db")
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "mamba_mentality")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+app = FastAPI(title="FastAPI Pet UI ")
+
+templates = Jinja2Templates(directory="app/templates")
 
 
-@app.get('/')
-def read_root():
+def get_db():
+    connection = psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
     try:
-        conn=psycopg2.connect(host=DB_HOST, database=DB_NAME, password=DB_PASSWORD,
-                              user=DB_USER,)
-        conn.close()
-        db_status = "Успішно підключено до PostgreSQL!)))"
+        yield connection
+    finally:
+        connection.close()
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request, db=Depends(get_db)):
+    try:
+        with db.cursor() as cursor:
+            cursor.execute("SELECT 1;")
+            db_status = "CONNECTED"
     except Exception as e:
-        db_status = f"Помилка підключення до БД: {str(e)}"
-    return {
-        "status": "Додаток працює",
-        "database": db_status
-    }
-        
+        db_status = f"ERROR: {str(e)}"
+
+    return templates.TemplateResponse(
+        "index.html", 
+        {"request": request, "db_status": db_status}
+    )
+
+
+@app.post("/api/records")
+async def add_record(db=Depends(get_db)):
+    return {"status": "success", "message": "Record added successfully"}
